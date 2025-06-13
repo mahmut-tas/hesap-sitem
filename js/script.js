d57674c35bb2ffdcd18f3e3acb9c7ff673d02bb2
mahmut.tas <mahmut.tas90@gmail.com>
2025-06-13 16:15:33 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Hesapla Pratik ana sayfası yüklendi!");
 
-    // Örnek: Smooth scroll için (navigasyondaki #calculator-list linki için)
-    const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
+    // Örnek: Smooth scroll için
+    // Sadece 'smooth-scroll' sınıfına sahip A etiketlerini dinle
+    const smoothScrollLinks = document.querySelectorAll('a.smooth-scroll[href^="#"]'); 
+    
     for (let link of smoothScrollLinks) {
         link.addEventListener('click', function (e) {
-            e.preventDefault();
+            e.preventDefault(); // Varsayılan link davranışını engelle
             let targetId = this.getAttribute('href');
-            if (targetId.length > 1 && document.querySelector(targetId)) { // Hedefin varlığını kontrol et
-                 document.querySelector(targetId).scrollIntoView({
-                    behavior: 'smooth'
+            
+            // Hedef ID'nin varlığını kontrol et ve boş değilse kaydır
+            if (targetId.length > 1 && document.querySelector(targetId)) { 
+                document.querySelector(targetId).scrollIntoView({
+                    behavior: 'smooth' // Yumuşak kaydırma efekti
                 });
             }
         });
